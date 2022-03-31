50ac3ed2dbdd683e3eee88eeee1e8b39ab956455
Unshelled12 <enpeachey@gmail.com>
2022-03-31 14:10:04 -0400

    Started DriverControlled file 3/31/22

---
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriverControlled.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(group = "drive")
+public class DriverControlled extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+
+    }
+}
