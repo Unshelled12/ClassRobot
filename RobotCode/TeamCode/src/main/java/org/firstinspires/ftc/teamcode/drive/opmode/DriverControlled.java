5f41ca4bcd4143bcd5b2ab9b4f108c67e30a90b3
Unshelled12 <enpeachey@gmail.com>
2022-04-26 16:20:35 -0400

    Added controls for the hook and launch motor.

--- RobotCode/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriverControlled.java
+++ RobotCode/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriverControlled.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 @TeleOp(group = "drive")
@@ -11,20 +12,29 @@ public class DriverControlled extends LinearOpMode {
     private DcMotor leftRear;
     private DcMotor rightFront;
     private DcMotor rightRear;
+    private DcMotor launch;
+
+    private Servo hook;
 
     public void runOpMode() throws InterruptedException{
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         leftRear = hardwareMap.get(DcMotor.class, "leftRear");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+        launch = hardwareMap.get(DcMotor.class, "launch");
+
+        hook = hardwareMap.get(Servo.class, "hook");
 
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftRear.setDirection(DcMotor.Direction.REVERSE);
+
+        hook.setPosition(0.35);
 
         waitForStart();
 
         while(!isStopRequested())
         {
+            //Drive Controls
             double y = gamepad1.left_stick_y;
             double strafe = -gamepad1.left_stick_x;
             double spin = gamepad1.right_stick_x;
@@ -33,6 +43,27 @@ public class DriverControlled extends LinearOpMode {
             leftRear.setPower(y - strafe + spin);
             rightFront.setPower(y - strafe - spin);
             rightRear.setPower(y + strafe - spin);
+
+            //set hook to down position
+            if(gamepad1.a)
+            {
+                hook.setPosition(0);
+            }
+            //set hook to up position
+            else if(gamepad1.y)
+            {
+                hook.setPosition(0.35);
+            }
+
+            //spin launch motor
+            if(gamepad1.b)
+            {
+                launch.setPower(-1);
+            }
+            else
+            {
+                launch.setPower(0);
+            }
         }
     }
 }
